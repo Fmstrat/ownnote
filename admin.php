b0b00a7f1a19474c2480279773d65cd0208f04bd
Fmstrat <github@nosolutions.com>
2016-03-10 11:20:40 -0500

    Make Database only Default

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 OCP\User::checkAdminUser();
 
 $tmpl = new OCP\Template('ownnote', 'admin');
-$tmpl->assign('folder', OCP\Config::getAppValue('ownnote', 'folder', 'Notes'));
+$tmpl->assign('folder', OCP\Config::getAppValue('ownnote', 'folder', ''));
 $tmpl->assign('disableAnnouncement', OCP\Config::getAppValue('ownnote', 'disableAnnouncement', ''));
 
 return $tmpl -> fetchPage();
